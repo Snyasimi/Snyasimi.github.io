f2a5020a2e9d8e241058dc1efeb997e779dd99fa
Snyasimi <nyasimisolo@gmail.com>
2024-05-06 14:24:03 +0300

    Finished adding js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 
 const API_KEY = "EjUoC9JtUKrteFF8ZUBjbcUUMXfwcgii9SOuIr8r";
 
-const IMAGE_URL =`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=5`;
+const IMAGE_URL =`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=10`;
 //Take in the imageobj as argument
 
 function createCard(imageobj){
@@ -20,14 +20,30 @@ function createCard(imageobj){
 		let figure = document.createElement("figure");
 		let caption = document.createElement("figcaption");
 		let image = document.createElement("img");
+		let link = document.createElement('a');
+		
+
+		link.href= imageobj['url'];
+		link.textContent = "View Original image";
+		
 		
 		image.src = imageobj['url'];
+		image.alt = imageobj['title'];
 		imageContainer.className = "image-container";
 		
+		figure.width="100%";
+		
+
+
 		imageContainer.appendChild(figure)
 			      .appendChild(image);
 
-		caption.textContent = imageobj['title']; 
+		//imageContainer.appendChild(link);
+
+		caption.innerHTML = `<p>TITLE: ${imageobj['title']},</p>
+							  <p> DATE: ${imageobj['date']},\ </p>
+							   <p>COPYRIGHT: ${imageobj['copyright']}.</p>`;
+
 		figure.appendChild(caption);
 
 
@@ -35,7 +51,6 @@ function createCard(imageobj){
 		imageGallery.appendChild(imageContainer);
 	
 	
-		return true;
 	}
 
 	catch(error){
